645c458f7586ba6cbe2546a0814d115e1ad9474d
Chris Nunez <cenunez47@gmail.com>
2017-10-12 23:24:35 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,3 +2,15 @@
 function paragraphSelector() {
   return $('p');
 }
+
+function lastImageSelector() {}
+
+function ninjaBabySelector() {}
+
+function divSelector() {
+  $('div');
+}
+
+function firstListItem() {
+  $('ul li:first');
+}
